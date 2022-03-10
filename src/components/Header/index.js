974b485fa11045d874617e9e208d298d12605316
Joel Kovalcson <joelkovalcson@gmail.com>
2022-03-10 16:07:34 -0500

    Setup header with working nav bar

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,10 +4,13 @@ import Navigation from '../Navigation';
 const Header = () => {
 	return (
 		<header>
-			<h1>JOEL KOVALCSON</h1>
-			<Navigation>
-
-			</Navigation>
+			<section className='sm:flex sm:flex-wrap px-2 py-3 bg-darkgrey text-orange-hl'>
+				<h1 className="text-2xl font-bold text-center sm:grow sm:text-left">JOEL KOVALCSON</h1>
+				<Navigation/>
+			</section>
+			<div
+				className="bg-banner h-20 bg-cover"
+			/>
 		</header>
 	);
 }
--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -3,18 +3,18 @@ import {Link} from 'react-router-dom';
 
 const Navigation = () => {
 	return (
-		<nav>
-			<ul>
-				<li>
+		<nav className='grow pt-1'>
+			<ul className='flex flex-nowrap text-xl justify-center sm:justify-end '>
+				<li className='mx-3'>
 					<Link to="/">About</Link>
 				</li>
-				<li>
+				<li className='mx-3'>
 					<Link to="/projects">Projects</Link>
 				</li>
-				<li>
+				<li className='mx-3'>
 					<Link to="/contact">Contact Me</Link>
 				</li>
-				<li>
+				<li className='mx-3'>
 					<Link to="/resume">Resume</Link>
 				</li>
 			</ul>
