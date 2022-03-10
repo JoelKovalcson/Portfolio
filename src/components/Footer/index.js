88a2591accd8cb51a61b0147496d2f2b28768c14
Joel Kovalcson <joelkovalcson@gmail.com>
2022-03-10 16:08:07 -0500

    Setup basic image links to social platforms

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -3,7 +3,26 @@ import React from 'react';
 const Footer = () => {
 	return (
 		<footer>
-			Footer Content
+			<ul className='flex flex-wrap w-auto justify-center py-2'>
+				<li className='mx-5'>
+					<a href="https://www.linkedin.com/in/joel-kovalcson-6670b5206/">
+						<img 
+							src={require('../../assets/linkedin-logo.png')} 
+							alt="LinkedIn Logo"
+							className="h-10"
+						/>
+					</a>
+				</li>
+				<li className='mx-5'>
+					<a href="https://github.com/JoelKovalcson">
+						<img
+							src={require('../../assets/github-logo.png')}
+							alt="GitHub Logo"
+							className="h-10"
+						/>
+					</a>
+				</li>
+			</ul>
 		</footer>
 	);
 }
