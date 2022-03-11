11aabdc766e5c2dfc0cb02565b156d77698c1949
Joel Kovalcson <joelkovalcson@gmail.com>
2022-03-10 20:04:32 -0500

    Small formatting changes in header/footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -4,20 +4,20 @@ const Footer = () => {
 	return (
 		<footer>
 			<ul className='flex flex-wrap w-auto justify-center py-2'>
-				<li className='mx-5'>
-					<a href="https://www.linkedin.com/in/joel-kovalcson-6670b5206/">
-						<img 
-							src={require('../../assets/linkedin-logo.png')} 
-							alt="LinkedIn Logo"
-							className="h-10"
-						/>
-					</a>
-				</li>
-				<li className='mx-5'>
+			<li className='mx-3'>
 					<a href="https://github.com/JoelKovalcson">
 						<img
 							src={require('../../assets/github-logo.png')}
 							alt="GitHub Logo"
+							className="h-10"
+						/>
+					</a>
+				</li>
+				<li className='mx-3'>
+					<a href="https://www.linkedin.com/in/joel-kovalcson-6670b5206/">
+						<img 
+							src={require('../../assets/linkedin-logo.png')} 
+							alt="LinkedIn Logo"
 							className="h-10"
 						/>
 					</a>
--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -5,16 +5,19 @@ const Navigation = () => {
 	return (
 		<nav className='grow pt-1'>
 			<ul className='flex flex-nowrap text-xl justify-center sm:justify-end '>
-				<li className='mx-3'>
+				<li className='mx-1 sm:mx-3'>
 					<Link to="/">About</Link>
 				</li>
-				<li className='mx-3'>
+				|
+				<li className='mx-1 sm:mx-3'>
 					<Link to="/projects">Projects</Link>
 				</li>
-				<li className='mx-3'>
+				|
+				<li className='mx-1 sm:mx-3'>
 					<Link to="/contact">Contact Me</Link>
 				</li>
-				<li className='mx-3'>
+				|
+				<li className='mx-1 sm:mx-3'>
 					<Link to="/resume">Resume</Link>
 				</li>
 			</ul>
