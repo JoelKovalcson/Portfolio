8f6c1cb432dae289bab2846a75861be05cbab9b1
Joel Kovalcson <joelkovalcson@gmail.com>
2022-03-10 22:16:16 -0500

    Updated About page and navigation to be more mobile friendly

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -4,20 +4,20 @@ import {Link} from 'react-router-dom';
 const Navigation = () => {
 	return (
 		<nav className='grow pt-1'>
-			<ul className='flex flex-nowrap text-xl justify-center sm:justify-end '>
-				<li className='mx-1 sm:mx-3'>
+			<ul className='text-center sm:flex sm:flex-nowrap text-xl justify-center sm:justify-end'>
+				<li className='mx-1 sm:mx-2'>
 					<Link to="/">About</Link>
 				</li>
-				|
-				<li className='mx-1 sm:mx-3'>
+				<span className='hidden sm:inline'>|</span>
+				<li className='mx-1 sm:mx-2'>
 					<Link to="/projects">Projects</Link>
 				</li>
-				|
-				<li className='mx-1 sm:mx-3'>
+				<span className='hidden sm:inline'>|</span>
+				<li className='mx-1 sm:mx-2'>
 					<Link to="/contact">Contact Me</Link>
 				</li>
-				|
-				<li className='mx-1 sm:mx-3'>
+				<span className='hidden sm:inline'>|</span>
+				<li className='mx-1 sm:mx-2'>
 					<Link to="/resume">Resume</Link>
 				</li>
 			</ul>
--- src/pages/About.js
+++ src/pages/About.js
@@ -5,11 +5,11 @@ const About = () => {
 	return (
 		<main className='mx-3 sm:mx-15 my-5 flex flex-wrap lg:flex-nowrap justify-center p-5 border-4 rounded-lg border-slate-500'>
 			<img
-				className='mb-3 lg:mb-0 lg:mr-5 h-80'
+				className='mb-3 lg:mb-0 lg:mr-5 max-h-80 aspect-square'
 				src={require('../assets/me.png')}
 				alt="Joel Kovalcson's Headshot"
 			/>
-			<p className='text-darkgrey text-xl'>
+			<p className='text-darkgrey text-l sm:text-xl'>
 				Reminiscing on my childhood, I was always fascinated by technology in various mediums ranging from game
 				systems to personal computers. To me these devices were simply toys at the time, something to play a
 				game on. Lo and behold, I later realized that my methodical and persistent nature were helping me
