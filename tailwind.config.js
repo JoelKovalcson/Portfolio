843d1b2edb67b3735b6e1a3fb466ac6c33f2c7c9
Joel Kovalcson <joelkovalcson@gmail.com>
2022-03-24 22:09:16 -0400

    Added Catro Gaming background image

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,8 @@ module.exports = {
 			backgroundImage: {
 				'banner': "url('/src/assets/banner-background.jpg')",
 				'programmer-dashboard': "url('/src/assets/projects/programmer-dashboard.png')",
-				'chattleship': "url('/src/assets/projects/chattleship.png')"
+				'chattleship': "url('/src/assets/projects/chattleship.png')",
+				'catro-gaming': "url('/src/assets/projects/catro-gaming.png')"
 			},
 			colors: {
 				'offwhite': '#dddddd',
